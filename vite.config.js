e43edac11f3b607e736e9297d7131337cb13f863
Kaniel Kirby <kanielrkirby@gmail.com>
2022-11-17 12:59:45 -0600

    added base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import postCssNested from 'postcss-nested'
 import customMedia from 'postcss-custom-media'
 
 export default defineConfig({
+	base: '/PolyChrome/',
 	css: {
 		postcss: {
 			plugins: [postCssNested, customMedia],
