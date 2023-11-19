61f1091c78bc741003832f25ea80085667dfae82
Piratey7007 <piratey7007@runbox.com>
2023-11-19 11:29:05 -0600

    Cleanup type errors on palette.ts

--- src/scripts/palette.ts
+++ src/scripts/palette.ts
@@ -1,11 +1,12 @@
 import colorAlgorithms from './colorAlgorithms'
 import { deconstructHex, local } from './utils'
 import { v4 as uuidv4 } from 'uuid'
+import Palette from './palette'
 
-let pal = document.getElementById('palette')!
+const pal = document.getElementById('palette') as HTMLElement
 
 export type Slot = {
-	palette: any
+	palette: Palette
 	hex: string
 	isLocked: boolean
 	id: string
@@ -24,22 +25,22 @@ export default class {
 		active: false,
 		show(index: number) {
 			if (!this.active) {
-				let plusDiv = this.render(index.toString())
+				const plusDiv = this.render(index.toString())
 				let slot = this.palette.slots[index]
 				if (!slot) {
 					slot = this.palette.slots[index + 1]
-					let contextDiv = document.getElementById(slot.id)
+					const contextDiv = document.getElementById(slot.id)
 					contextDiv?.before(plusDiv)
 				} else {
-					let contextDiv = document.getElementById(slot.id)
-					contextDiv!.after(plusDiv)
+					const contextDiv = document.getElementById(slot.id) as HTMLElement
+					contextDiv.after(plusDiv)
 				}
 				this.active = true
 			}
 		},
 		hide(amount?: number) {
 			if (this.active) {
-				document.getElementById('plus-button')?.parentElement!.parentElement!.remove()
+				(((document.getElementById('plus-button') as HTMLElement)?.parentElement as HTMLElement)?.parentElement as HTMLElement)?.remove()
 				this.active = false
 				if (this.disabled) return
 				this.disabled = true
@@ -47,24 +48,24 @@ export default class {
 					() => {
 						this.disabled = false
 					},
-					typeof amount == 'number' ? amount : 300
+					typeof amount === 'number' ? amount : 300
 				)
 			}
 		},
 		render(data: string) {
 			// invisible container
-			let div = document.createElement('div')
+			const div = document.createElement('div')
 			div.classList.add('plus-container')
 			// circle
-			let circle = document.createElement('div')
+			const circle = document.createElement('div')
 			circle.classList.add('circle')
 			// svg
-			let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+			const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
 			svg.setAttribute('data-color-index', data)
 			svg.setAttribute('viewBox', '0 0 100 100')
 			svg.id = 'plus-button'
 			// use
-			let use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
+			const use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
 			use.setAttribute('href', '#icon-plus-button')
 			use.setAttribute('viewBox', '0 0 100 100')
 
@@ -78,9 +79,9 @@ export default class {
 	algorithm = {
 		list: colorAlgorithms,
 		get(type?: string) {
-			if (type == 'slot') this.details = 'slot'
+			if (type === 'slot') this.details = 'slot'
 			else this.details = ''
-			let index = local.settings.algorithm
+			const index = local.settings.algorithm
 			return this.list[index]
 		},
 		details: '',
@@ -96,13 +97,12 @@ export default class {
 		},
 	}
 
-	removeSlot(slot: Slot, options?: { animations: boolean }) {
-		if (!options) options = { animations: true }
-		let startingIndex = slot.data + 1
+	removeSlot(slot: Slot, options: { animations: boolean } = { animations: true }) {
+		const startingIndex = slot.data + 1
 		for (let i = startingIndex; i < this.slots.length; i++) this.slots[i].data--
-		let slotDiv = document.getElementById(slot.id)!
+		const slotDiv = document.getElementById(slot.id) as HTMLElement
 		this.slots.splice(slot.data, 1)
-		if (options.animations == true) {
+		if (options.animations === true) {
 			slotDiv.classList.add('deinit')
 			function deInit() {
 				slotDiv.remove()
@@ -119,42 +119,43 @@ export default class {
 		slot: { hex?: string; id?: string; isLocked?: boolean; index?: number | undefined },
 		options?: { animations?: boolean }
 	) {
-		let newSlot = {
+		const newSlot = {
 			palette: this,
-			hex: slot.hex!,
-			isLocked: typeof slot.isLocked == 'boolean' ? slot.isLocked : false,
+			hex: slot.hex as string,
+			isLocked: typeof slot.isLocked === 'boolean' ? slot.isLocked : false,
 			id: slot.id || btoa(parseInt(uuidv4(), 16).toString(36)).replaceAll('=', ''),
-			index: typeof slot.index != 'number' ? this.slots.length : slot.index,
+			index: typeof slot.index !== 'number' ? this.slots.length : slot.index,
 			get data() {
-				let d = parseInt(document.getElementById(this.id)?.getAttribute('data-color-index')!)
-				if (typeof d == 'number') return d
-				return (this.data = this.index)
+				const d = parseInt(document.getElementById(this.id)?.getAttribute('data-color-index') as string)
+				if (typeof d === 'number') return d
+				this.data = this.index
+        return this.index
 			},
 			set data(d: number) {
 				document.getElementById(this.id)?.setAttribute('data-color-index', d.toString())
 				this.index = d
 			},
 			generate() {
-				this.hex = this.palette.algorithm.get('slot')(this.palette, this.index)!
+				this.hex = this.palette.algorithm.get('slot')(this.palette, this.index) as string
 				this.sync()
 			},
 			sync() {
-				let div = document.getElementById(this.id)!
+				const div = document.getElementById(this.id) as HTMLElement
 				if (this.hex) {
-					let { lum } = deconstructHex(this.hex)
+					const { lum } = deconstructHex(this.hex)
 					if (lum < 0.5) {
 						div.classList.add('isLight')
 					} else {
 						div.classList.remove('isLight')
 					}
-					div.style.backgroundColor = '#' + this.hex
-					div.getElementsByClassName('hex')[0].innerHTML = '#' + this.hex
+					div.style.backgroundColor = `#${this.hex}`
+					div.getElementsByClassName('hex')[0].innerHTML = `#${this.hex}`
 				}
 			},
 		}
-		if (!newSlot.hex) newSlot.hex = this.algorithm.get('slot')(this, newSlot.index)!
-		let div = this.createSwatch(newSlot, options)
-		let prevDiv = document.getElementById(this.slots[newSlot.index - 1]?.id)
+		if (!newSlot.hex) newSlot.hex = this.algorithm.get('slot')(this, newSlot.index) as string
+		const div = this.createSwatch(newSlot, options)
+		const prevDiv = document.getElementById(this.slots[newSlot.index - 1]?.id)
 		if (!prevDiv) pal.prepend(div)
 		else prevDiv.after(div)
 		newSlot.data = newSlot.index
@@ -163,16 +164,15 @@ export default class {
 		return newSlot
 	}
 
-	createSwatch(slot: any, options?: { animations?: boolean }) {
-		if (!options) options = { animations: true }
+	createSwatch(slot: Slot, options: { animations?: boolean } = { animations: true }): HTMLElement {
 		// Div Element
-		let div = document.createElement('div')
+		const div = document.createElement('div')
 		div.classList.add('swatch')
-		div.setAttribute('data-color-index', slot.data)
+		div.setAttribute('data-color-index', slot.data.toString())
 		div.id = slot.id
 		if (slot.hex) {
-			div.style.backgroundColor = '#' + slot.hex
-			let { lum } = deconstructHex(slot.hex)
+			div.style.backgroundColor = `#${slot.hex}`
+			const { lum } = deconstructHex(slot.hex)
 			if (lum < 0.5) {
 				div.classList.add('isLight')
 			} else {
@@ -180,7 +180,7 @@ export default class {
 			}
 		}
 
-		if (options.animations == true) {
+		if (options.animations === true) {
 			function init() {
 				div.classList.remove('init')
 				div.removeEventListener('animationend', init)
@@ -192,24 +192,24 @@ export default class {
 		}
 
 		// Center
-		let center = document.createElement('div')
+		const center = document.createElement('div')
 		center.classList.add('options')
 
 		// X SVG
-		let x = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+		const x = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
 		x.classList.add('icon', 'x')
 		x.setAttribute('viewBox', '0 0 100 100')
-		let xUse = document.createElementNS('http://www.w3.org/2000/svg', 'use')
+		const xUse = document.createElementNS('http://www.w3.org/2000/svg', 'use')
 		xUse.setAttribute('href', '#icon-x')
 
 		// Lock SVG
-		let lockBody = document.createElementNS('http://www.w3.org/2000/svg', 'use')
+		const lockBody = document.createElementNS('http://www.w3.org/2000/svg', 'use')
 		lockBody.setAttribute('viewBox', '0 0 100 100')
 		lockBody.setAttribute('href', '#icon-lock-body')
-		let lockArm = document.createElementNS('http://www.w3.org/2000/svg', 'use')
+		const lockArm = document.createElementNS('http://www.w3.org/2000/svg', 'use')
 		lockArm.setAttribute('viewBox', '0 0 100 100')
 		lockArm.setAttribute('href', '#icon-lock-arm')
-		let lock = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+		const lock = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
 		lock.setAttribute('viewBox', '0 0 100 100')
 		lock.classList.add('icon', 'lock')
 		if (slot.isLocked) lock.classList.add('locked')
