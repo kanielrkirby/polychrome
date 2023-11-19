3e4bb4d47c4b57de40108cb5d827e18507a08cb2
Piratey7007 <piratey7007@runbox.com>
2023-11-19 11:29:12 -0600

    Cleanup type errors on routing.ts

--- src/scripts/routing.ts
+++ src/scripts/routing.ts
@@ -1,23 +1,23 @@
 export default class {
-	constructor(views: [string, Function][]) {
+	constructor(views: [string, (ids?: string[]) => void][]) {
 		this.routes = new Map(views)
 	}
 
-	routes: Map<string, Function>
+	routes: Map<string, (ids?: string[]) => void>
 
-	loadContent = (url?: any) => {
+	loadContent = (url?: { base: string; ids?: string[], site: string }) => {
 		if (url) {
 			let { base, ids } = url
-			if (typeof base != 'string') base = url
-			let route = this.routes.get(base)!
+			if (typeof base !== 'string') base = ''
+			const route = this.routes.get(base) as (ids?: string[]) => void
 			if (ids) route(ids)
 			else route()
-		} else this.routes.get('404')!()
+		} else (this.routes.get('404') as (ids?: string[]) => void)()
 	}
 
 	routeToURL = (url: string) => {
-		let deconURL = this.deconstructURL(url)
-		let { base, ids } = deconURL
+		const deconURL = this.deconstructURL(url)
+		const { base, ids } = deconURL
 		if (this.routes.has(base)) {
 			if (base === 'create') {
 				if (ids) {
@@ -28,30 +28,29 @@ export default class {
 		} else this.loadContent()
 	}
 
-	navigateTo = (url: string, replace?: boolean) => {
-		replace = replace || false
+	navigateTo = (url: string, replace = false) => {
 		if (replace) history.replaceState('', '', url)
-		else history.pushState('', '', '/polychrome' + url)
+		else history.pushState('', '', `/polychrome${url}`)
 		this.routeToURL(url)
 	}
 
 	deconstructURL(url: string) {
-		let array = url.replace(/^\//, '').replace(/\/$/, '').split('/')
-		if (array[0] == 'polychrome')
+		const array = url.replace(/^\//, '').replace(/\/$/, '').split('/')
+		if (array[0] === 'polychrome')
 			return {
 				site: 'polychrome',
 				base: array[1] || '',
-				ids: array[2] ? array[2].split('-') : null,
+				ids: array[2] ? array[2].split('-') : undefined,
 			}
 		return {
 			site: 'polychrome',
 			base: array[0],
-			ids: array[1] ? array[1].split('-') : null,
+			ids: array[1] ? array[1].split('-') : undefined,
 		}
 	}
 
 	idsAreValid(ids: string[]) {
-		for (let id of ids) if (!/^[a-f0-9]{6}$/.test(id) || ids.length > 10) return false
+		for (const id of ids) if (!/^[a-f0-9]{6}$/.test(id) || ids.length > 10) return false
 		return true
 	}
 }
